7b56249b3a92fdf0e0483492e2df21ec73659c8f
joshjordans <mulondojoshua71@gmail.com>
2026-09-15 13:03:41 +0300

    SORT IMPLEMENTATION

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,1 +1,18 @@
-//Group 12 - sort() implementation
+// Group 12 - sort() implementation
+
+void sort(int scores[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (scores[j] > scores[j + 1])
+            {
+                int temp = scores[j];
+                scores[j] = scores[j + 1];
+                scores[j + 1] = temp;
+            }
+        }
+    }
+}
+
