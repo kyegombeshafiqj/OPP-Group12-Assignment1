7543c6023e091fcbf65f49da665e15af163bf2fe
Mabanda Eriya Nesta <nesiestarvil@gmail.com>
2026-09-16 18:20:28 +0300

    Implement dense rank operation

--- src/dense_rank.cpp
+++ src/dense_rank.cpp
@@ -1,1 +1,35 @@
-//Group 12 - dense_rank() implementation
+// Group 12 - dense_rank() implementation
+
+#include "../include/ranking_sorting.h"
+
+void denseRankData(int scores[], int ranks[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int rank = 1;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (scores[j] < scores[i])
+            {
+                bool alreadyCounted = false;
+
+                for (int k = 0; k < j; k++)
+                {
+                    if (scores[k] == scores[j])
+                    {
+                        alreadyCounted = true;
+                        break;
+                    }
+                }
+
+                if (!alreadyCounted)
+                {
+                    rank++;
+                }
+            }
+        }
+
+        ranks[i] = rank;
+    }
+}
